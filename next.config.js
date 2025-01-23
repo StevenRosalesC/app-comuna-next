45e3af84142acafb61e0b3b5ac19409d8656b74e
Steven Rosales <stevenrosales31@gmail.com>
2025-01-22 19:24:54 -0500

    Refactor next.config.js to remove commented-out image remote patterns and set output to standalone

--- next.config.js
+++ next.config.js
@@ -2,41 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'utfs.io',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'api.slingacademy.com',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'secure.gravatar.com',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'avatar-management--avatars.us-west-2.prod.public.atl-paas.net',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'ik.imagekit.io',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'flowbite.s3.amazonaws.com',
-      //   port: ''
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'i.ibb.co',
-      //   port: ''
-      // },
   {
         protocol: 'https', 
         hostname: '**', 
@@ -48,6 +13,7 @@ const nextConfig = {
     ]
   },
   transpilePackages: ['geist'],
+  output:'standalone',
 };
 
 module.exports = nextConfig;

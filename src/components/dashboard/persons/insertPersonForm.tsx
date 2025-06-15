f30cdd7b14707ff93deb1c49ec4c655b05189d19
Steven Rosales <stevenrosales31@gmail.com>
2025-06-14 19:16:14 -0500

    Update(insertPersonForm.tsx, persons.view.tsx):
    Enhanced person insertion functionality by...
    - Updated import statements to use single quotes for consistency.
    - Improved form validation messages for better user feedback.
    - Refactored form structure for improved readability and maintainability.
    - Integrated neighborhood data fetching and permissions check in the dashboard view.
    - Enhanced loading state management for neighborhood selection.

--- src/components/dashboard/persons/insertPersonForm.tsx
+++ src/components/dashboard/persons/insertPersonForm.tsx
@@ -1,85 +1,91 @@
-"use client"
+'use client';
 
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "@/components/ui/button"
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "sonner"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { cn } from "@/lib/utils"
-import { CalendarIcon } from "lucide-react"
-import { personsService } from "@/services/persons"
-import { IPerson } from "@/interfaces/persons"
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { toast } from 'sonner';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { CalendarIcon } from 'lucide-react';
+import { personsService } from '@/services/persons';
+import { IPerson } from '@/interfaces/persons';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Events } from "@/interfaces/enums"
-import { useNeighborhoodsStore } from "@/hooks/store/useNeighborhoodsStore"
-import { useEffect } from "react"
-import { Neighborhood } from "@/store/neighborhoodsStore"
-import { Skeleton } from "@/components/ui/skeleton"
+  SelectValue
+} from '@/components/ui/select';
+import { Events } from '@/interfaces/enums';
+import { Neighborhood } from '@/store/neighborhoodsStore';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const formSchema = z.object({
-  identification: z.string().min(1, "La cédula es requerida"),
-  firstName: z.string().min(1, "El nombre es requerido"),
-  lastName: z.string().min(1, "El apellido es requerido"),
-  email: z.string().email("Email inválido").optional().or(z.literal("")),
-  gender: z.number().min(0, "El género es requerido").max(1, "El género debe ser 0 o 1"),
-  birthDate: z.string().min(1, "La fecha de nacimiento es requerida"),
+  identification: z.string().min(1, 'La cédula es requerida'),
+  firstName: z.string().min(1, 'El nombre es requerido'),
+  lastName: z.string().min(1, 'El apellido es requerido'),
+  email: z.string().email('Email inválido').optional().or(z.literal('')),
+  gender: z
+    .number()
+    .min(0, 'El género es requerido')
+    .max(1, 'El género debe ser 0 o 1'),
+  birthDate: z.string().min(1, 'La fecha de nacimiento es requerida'),
   phone: z.string().optional(),
   address: z.string().optional(),
-  neighborhoodId: z.string().optional(),
-})
+  neighborhoodId: z.string().optional()
+});
 
-type FormValues = z.infer<typeof formSchema>
+type FormValues = z.infer<typeof formSchema>;
 
-export default function InsertPersonForm() {
+export default function InsertPersonForm({
+  neighborhoods,
+  isLoading
+}: {
+  neighborhoods: Neighborhood[];
+  isLoading: boolean;
+}) {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      identification: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      birthDate: "",
-      phone: "",
-      address: "",
+      identification: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      birthDate: '',
+      phone: '',
+      address: '',
       gender: 1,
-      neighborhoodId: "",
-    },
-  })
-  const { neighborhoods, isLoading, fetchNeighborhoods } = useNeighborhoodsStore((state) => ({
-    neighborhoods: state.neighborhoods,
-    isLoading: state.isLoading,
-    fetchNeighborhoods: state.fetchNeighborhoods
-  }))
-
-  useEffect(() => {
-    fetchNeighborhoods()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+      neighborhoodId: ''
+    }
+  });
 
   // Neighborhood options for Select
   // This variable is outside the JSX for better readability
-  const neighborhoodsOptions = neighborhoods?.map((neighborhood: Neighborhood) => (
-    <SelectItem key={neighborhood.neighborhoodId} value={neighborhood.neighborhoodId}>
-      {neighborhood.neighborhoodName}
-    </SelectItem>
-  ))
+  const neighborhoodsOptions = neighborhoods?.map(
+    (neighborhood: Neighborhood) => (
+      <SelectItem
+        key={neighborhood.neighborhoodId}
+        value={neighborhood.neighborhoodId}
+      >
+        {neighborhood.neighborhoodName}
+      </SelectItem>
+    )
+  );
 
   async function onSubmit(data: FormValues) {
     try {
@@ -89,11 +95,11 @@ export default function InsertPersonForm() {
         phoneNumber: data.phone,
         identification: data.identification,
         gender: data.gender ? 1 : 0,
-        birthDate: data.birthDate || "",
-        email: data.email || "",
-        neighborhoodId: data.neighborhoodId,
-      }
-      const response = await personsService.createPerson(person)
+        birthDate: data.birthDate || '',
+        email: data.email || '',
+        neighborhoodId: data.neighborhoodId
+      };
+      const response = await personsService.createPerson(person);
       if (response.status) {
         const event = new CustomEvent(Events.PERSONS_CREATED, {
           bubbles: true,
@@ -101,13 +107,13 @@ export default function InsertPersonForm() {
           detail: { person }
         });
         document.dispatchEvent(event);
-        toast.success("Persona guardada exitosamente")
-        form.reset()
+        toast.success('Persona guardada exitosamente');
+        form.reset();
       } else {
-        toast.error(response.message)
+        toast.error(response.message);
       }
     } catch (error) {
-      toast.error("Error al guardar la persona")
+      toast.error('Error al guardar la persona');
     }
   }
 
@@ -118,16 +124,16 @@ export default function InsertPersonForm() {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
               <FormField
                 control={form.control}
-                name="identification"
+                name='identification'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Cédula</FormLabel>
                     <FormControl>
-                      <Input placeholder="Ingrese la cédula" {...field} />
+                      <Input placeholder='Ingrese la cédula' {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -136,12 +142,12 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="firstName"
+                name='firstName'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Nombres</FormLabel>
                     <FormControl>
-                      <Input placeholder="Ingrese los nombres" {...field} />
+                      <Input placeholder='Ingrese los nombres' {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -150,33 +156,40 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="lastName"
+                name='lastName'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Apellidos</FormLabel>
                     <FormControl>
-                      <Input placeholder="Ingrese los apellidos" {...field} />
+                      <Input placeholder='Ingrese los apellidos' {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              {isLoading && <Skeleton className="h-10 w-full" />}
+              {isLoading && <Skeleton className='h-10 w-full' />}
               <FormField
                 control={form.control}
-                name="neighborhoodId"
+                name='neighborhoodId'
                 disabled={isLoading}
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Barrio</FormLabel>
-                    <Select onValueChange={(value) => field.onChange(value)} defaultValue={field.value}>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
                       <FormControl>
                         <SelectTrigger>
-                          <SelectValue placeholder="Seleccione el barrio" />
+                          <SelectValue placeholder='Seleccione el barrio' />
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        {isLoading ? <Skeleton className="h-10 w-full" /> : neighborhoodsOptions}
+                        {isLoading ? (
+                          <Skeleton className='h-10 w-full' />
+                        ) : (
+                          neighborhoodsOptions
+                        )}
                       </SelectContent>
                     </Select>
                     <FormMessage />
@@ -186,19 +199,22 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="gender"
+                name='gender'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Género</FormLabel>
-                    <Select onValueChange={(value) => field.onChange(Number(value))} defaultValue={field.value.toString()}>
+                    <Select
+                      onValueChange={(value) => field.onChange(Number(value))}
+                      defaultValue={field.value.toString()}
+                    >
                       <FormControl>
                         <SelectTrigger>
-                          <SelectValue placeholder="Seleccione el género" />
+                          <SelectValue placeholder='Seleccione el género' />
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="1">Masculino</SelectItem>
-                        <SelectItem value="0">Femenino</SelectItem>
+                        <SelectItem value='1'>Masculino</SelectItem>
+                        <SelectItem value='0'>Femenino</SelectItem>
                       </SelectContent>
                     </Select>
                     <FormMessage />
@@ -208,14 +224,14 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="email"
+                name='email'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
                       <Input
-                        type="email"
-                        placeholder="Ingrese el email"
+                        type='email'
+                        placeholder='Ingrese el email'
                         {...field}
                       />
                     </FormControl>
@@ -226,35 +242,38 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="birthDate"
+                name='birthDate'
                 render={({ field }) => (
-                  <FormItem className="flex flex-col gap-2">
+                  <FormItem className='flex flex-col gap-2'>
                     <FormLabel>Fecha de Nacimiento</FormLabel>
                     <Popover>
                       <PopoverTrigger asChild>
                         <FormControl>
                           <Button
-                            variant={"outline"}
+                            variant={'outline'}
                             className={cn(
-                              "w-full pl-3 text-left font-normal",
-                              !field.value && "text-muted-foreground"
+                              'w-full pl-3 text-left font-normal',
+                              !field.value && 'text-muted-foreground'
                             )}
                           >
                             {field.value ? (
-                              new Date(field.value).toLocaleDateString('es-ES', {
-                                day: '2-digit',
-                                month: 'long',
-                                year: 'numeric'
-                              })
+                              new Date(field.value).toLocaleDateString(
+                                'es-ES',
+                                {
+                                  day: '2-digit',
+                                  month: 'long',
+                                  year: 'numeric'
+                                }
+                              )
                             ) : (
                               <span>Seleccionar fecha</span>
                             )}
-                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
                           </Button>
                         </FormControl>
                       </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Input type="date" {...field} />
+                      <PopoverContent className='w-auto p-0' align='start'>
+                        <Input type='date' {...field} />
                       </PopoverContent>
                     </Popover>
                     <FormMessage />
@@ -264,15 +283,12 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="phone"
+                name='phone'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Teléfono</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder="Ingrese el teléfono"
-                        {...field}
-                      />
+                      <Input placeholder='Ingrese el teléfono' {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -281,15 +297,12 @@ export default function InsertPersonForm() {
 
               <FormField
                 control={form.control}
-                name="address"
+                name='address'
                 render={({ field }) => (
-                  <FormItem className="md:col-span-2">
+                  <FormItem className='md:col-span-2'>
                     <FormLabel>Dirección</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder="Ingrese la dirección"
-                        {...field}
-                      />
+                      <Input placeholder='Ingrese la dirección' {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -297,21 +310,19 @@ export default function InsertPersonForm() {
               />
             </div>
 
-            <div className="flex justify-end space-x-4">
+            <div className='flex justify-end space-x-4'>
               <Button
-                type="button"
-                variant="outline"
+                type='button'
+                variant='outline'
                 onClick={() => form.reset()}
               >
                 Cancelar
               </Button>
-              <Button type="submit">
-                Guardar
-              </Button>
+              <Button type='submit'>Guardar</Button>
             </div>
           </form>
         </Form>
       </CardContent>
     </Card>
-  )
+  );
 }

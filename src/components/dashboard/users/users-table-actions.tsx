e301ee0b7790bd30b630075eb4907561d5f8f6eb
Steven Rosales <stevenrosales31@gmail.com>
2025-06-14 19:30:41 -0500

    Update(permissions.ts, table-actions.tsx, users-table-actions.tsx):
    Enhanced permission handling in dashboard components by...
    - Updated permission constants to use 'UPDATE' instead of 'EDIT' for better clarity.
    - Integrated permission checks in UsersTableActions to conditionally render the edit button based on user permissions.
    - Improved code readability by organizing permission logic in the relevant components.

--- src/components/dashboard/users/users-table-actions.tsx
+++ src/components/dashboard/users/users-table-actions.tsx
@@ -6,6 +6,8 @@ import {
   TooltipTrigger
 } from '@/components/ui/tooltip';
 import { User } from '@/interfaces/users';
+import { usePermissionsStore } from '@/store/permissionsStore';
+import { ValidActions, ValidModules } from '@/constants/permissions';
 
 interface UsersTableActionsProps {
   user: User;
@@ -13,18 +15,24 @@ interface UsersTableActionsProps {
 }
 
 export function UsersTableActions({ user, onEdit }: UsersTableActionsProps) {
+  const { permissions } = usePermissionsStore();
+  const canUpdateUser = permissions?.[ValidModules.USERS]?.includes(
+    ValidActions.UPDATE
+  );
   return (
     <div className='flex items-center justify-end gap-2'>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant='ghost' size='icon' onClick={() => onEdit(user)}>
-            <Pencil className='h-4 w-4' />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Editar</p>
-        </TooltipContent>
-      </Tooltip>
+      {canUpdateUser && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant='ghost' size='icon' onClick={() => onEdit(user)}>
+              <Pencil className='h-4 w-4' />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Editar</p>
+          </TooltipContent>
+        </Tooltip>
+      )}
     </div>
   );
 }

4b394a22efe673ca7c18f783019d7a25c2a2fab2
Steven Rosales <stevenrosales31@gmail.com>
2025-06-19 20:37:36 -0500

    Update(loading.tsx, notices-datatable.tsx):
    Refactored loading and notices data table components for improved styling and structure.
    - Standardized quotation marks in loading component for consistency.
    - Adjusted layout and styling in loading component for better visual alignment.
    - Modified table header and cell widths in notices data table for enhanced readability.
    - Improved truncation handling in notices data table for better text display.

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function loading() {
   return (
-    <div className="relative flex justify-center items-center w-screen h-screen gap-5 dark:bg-gray-900">
-      <div className="flex justify-center items-center">
-        <div className="absolute animate-spin rounded-md h-32 w-32 border-4  border-emerald-500"></div>
+    <div className='relative flex h-screen w-screen items-center justify-center gap-5'>
+      <div className='flex items-center justify-center'>
+        <div className='absolute h-32 w-32 animate-spin rounded-md border-4  border-emerald-500'></div>
         <Image
-          src="/icon.webp"
-          className="rounded-full h-28 w-28 animate-horizontal-spin"
-          alt="Tailwindflex Logo"
+          src='/icon.webp'
+          className='h-28 w-28 animate-horizontal-spin rounded-full'
+          alt='Tailwindflex Logo'
           width={100}
           height={100}
         />
       </div>
-      <span className="text-2xl text-emerald-500">Comuna Bambil Collao, Espere un momento...</span>
-
+      <span className='text-2xl text-emerald-500'>
+        Comuna Bambil Collao, Espere un momento...
+      </span>
     </div>
-  )
+  );
 }

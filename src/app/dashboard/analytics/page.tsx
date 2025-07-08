050609c3f785548aca510511b1d9ae0e061bbad1
Steven Rosales <stevenrosales31@gmail.com>
2025-07-07 22:09:28 -0500

    refactor(analytics):
    Updated layout of AnalyticsPage for improved responsiveness and spacing. Adjusted container and grid classes to enhance visual structure and user experience.

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -2,8 +2,11 @@ import { SuperDataTable } from '@/components/dashboard/analytics/super-datatable
 
 export default function AnalyticsPage() {
   return (
-    <div className="container mx-auto py-6">
-      <SuperDataTable />
+    <div className='container mx-auto max-w-[1400px] space-y-6 px-2 py-4 sm:px-4'>
+      <div className='grid grid-cols-1 gap-6'>
+
+        <SuperDataTable />
+      </div>
     </div>
   );
-} 
+}
